9c96d7c8b377cd56d8bb83d517180aa659a6f6bf
Magnus Stavngaard <magnus@stavngaard.dk>
2015-06-07 17:07:49 -0400

    Fixed print of author

--- randomquote.py
+++ randomquote.py
@@ -12,4 +12,4 @@ with open(current_dir + '/quotes.json') as json_file:
 
     chosen_quote = random.choice(quote_list)
     print("\n".join(textwrap.wrap(chosen_quote['content'], 72)))
-    print('   -', chosen_quote['author'])
+    print('   -' + chosen_quote['author'])
